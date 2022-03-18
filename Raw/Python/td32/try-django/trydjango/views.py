2af8ffd0925c28245122d270035475bae7e3e20b
Kamil Wilczek <kamil.wilczek@igoriatrade.com>
2022-03-18 15:54:43 +0100

    12 - enriching a View with Data

--- Raw/Python/td32/try-django/trydjango/views.py
+++ Raw/Python/td32/try-django/trydjango/views.py
@@ -1,12 +1,8 @@
 """
 To render html web pages
 """
-
+import random
 from django.http import HttpResponse
-
-HTML_STRING = """
-<h1>Hello World</h1>
-"""
 
 
 def home_view(request):
@@ -14,4 +10,16 @@ def home_view(request):
     Take in a request (Django sedns request)
     Return HTML as a response (We pick to return the response)
     """
+
+    name = "Justin"
+    number = random.randint(
+        10, 1233123
+    )  # API call to some rest API with python & python requests
+    H1_STRING = f"""
+    <h1>Hello {name} - {number}!</h1>
+    """
+
+    P_STRING = f"""<p>Hi {name} - {number}!</p>"""
+
+    HTML_STRING = H1_STRING + P_STRING
     return HttpResponse(HTML_STRING)
