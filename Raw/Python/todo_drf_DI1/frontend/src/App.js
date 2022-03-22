52f840274a329420468fcaddec548fd6d169c4ec
Kamil Wilczek <kamilwilczek@wp.pl>
2022-03-22 21:41:08 +0100

    connecting react to django

--- Raw/Python/todo_drf_DI1/frontend/src/App.js
+++ Raw/Python/todo_drf_DI1/frontend/src/App.js
@@ -18,6 +18,9 @@ class App extends React.Component {
       this.handleChange = this.handleChange.bind(this)
       this.handleSubmit = this.handleSubmit.bind(this)
       this.getCookie = this.getCookie.bind(this)
+      this.startEdit = this.startEdit.bind(this)
+      this.deleteItem = this.deleteItem.bind(this)
+      this.strikeUnstrike = this.strikeUnstrike.bind(this)
   };
 
   getCookie(name) {
@@ -73,6 +76,14 @@ class App extends React.Component {
     var csrf_token = this.getCookie('csrftoken')
 
     var url = 'http://127.0.0.1:8000/api/task-create/'
+
+    if(this.state.editing === true){
+      url = `http://127.0.0.1:8000/api/task-update/${this.state.activeItem.id}/`
+      this.setState({
+        editing:false
+      })
+    }
+
     fetch(url, {
       method:'POST',
       headers:{
@@ -94,8 +105,49 @@ class App extends React.Component {
     })
   }
 
+  startEdit(task){
+    this.setState({
+      activeItem:task,
+      editing:true,
+    })
+  }
+
+  deleteItem(task){
+    var csrf_token = this.getCookie('csrftoken')
+
+    fetch(`http://127.0.0.1:8000/api/task-delete/${task.id}/`, {
+      method:'DELETE',
+      headers:{
+        'Content-type':'application/json',
+        'X-CSRFToken':csrf_token,
+      },
+    }).then((response) => {
+      this.fetchTasks()
+    })
+  }
+
+  strikeUnstrike(task){
+    task.completed = !task.completed
+    var csrf_token = this.getCookie('csrftoken')
+    var url = `http://127.0.0.1:8000/api/task-update/${task.id}/`
+
+    fetch(url, {
+      method:"POST",
+      headers:{
+        'Content-type':'application/json',
+        'X-CSRFToken':csrf_token,
+      },
+      body:JSON.stringify({'completed': task.completed, 'title': task.title})
+    }).then(() => {
+      this.fetchTasks()
+    })
+
+    console.log('TASK', task.completed)
+  }
+
   render(){
     var tasks = this.state.todoList
+    var self = this
 
     return(
       <div className='container'>
@@ -105,7 +157,7 @@ class App extends React.Component {
             <form onSubmit={this.handleSubmit} id='form'>
               <div className='flex-wrapper'>
                 <div style={{flex: 6}}>
-                  <input onChange={this.handleChange} className='form-control' id='title' type='text' name='title' placeholder='Add task...' />
+                  <input onChange={this.handleChange} className='form-control' id='title' value={this.state.activeItem.title} type='text' name='title' placeholder='Add task...' />
                 </div>
 
                 <div style={{flex: 1}}>
@@ -119,14 +171,21 @@ class App extends React.Component {
             {tasks.map(function(task, index){
               return(
                 <div key={index} className="task-wrapper flex-wrapper">
-                  <div style={{flex:7}}>
-                    <span>{task.title}</span>
+                  <div onClick={() => self.strikeUnstrike(task)} style={{flex:7}}>
+
+                    {task.completed === false ? (
+                      <span>{task.title}</span>
+                    ) : (
+                      <s>{task.title}</s>
+                    )}
+
+                    
                   </div>
                   <div style={{flex:1}}>
-                    <button className='btn btn-sm btn-outline-info'>Edit </button>
+                    <button onClick={() => self.startEdit(task)} className='btn btn-sm btn-outline-info'>Edit </button>
                   </div>
                   <div style={{flex:1}}>
-                  <button className='btn btn-sm btn-outline-dark delete'>-</button>
+                  <button onClick={() => self.deleteItem(task)} className='btn btn-sm btn-outline-dark delete'>-</button>
                   </div>
                   
                 </div>
