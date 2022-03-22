f594d38be39feb26f86ec2c7edec7317276ee4e7
Kamil Wilczek <kamil.wilczek@igoriatrade.com>
2022-03-22 10:02:22 +0100

    23 - Basic HTML Form in Django

--- Raw/Python/td32/try-django/articles/views.py
+++ Raw/Python/td32/try-django/articles/views.py
@@ -1,10 +1,10 @@
-from multiprocessing import context
 from django.shortcuts import render
 
 from articles.models import Article
 
 # Create your views here.
 def article_search_view(request):
+    print(request.GET)
     query_dict = request.GET  # this is a dictionary
     # query = query_dict.get("q")  # <input type="text" name="q"/>
 
@@ -18,6 +18,19 @@ def article_search_view(request):
         article_obj = Article.objects.get(id=query)
     context = {"object": article_obj}
     return render(request, "articles/search.html", context=context)
+
+
+def article_create_view(request, id=None):
+    # print(request.POST)
+    context = {}
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        print(title, content)
+        article_object = Article.objects.create(title=title, content=content)
+        context["object"] = article_object
+        context["created"] = True
+    return render(request, "articles/create.html", context=context)
 
 
 def article_detail_view(request, id=None):
