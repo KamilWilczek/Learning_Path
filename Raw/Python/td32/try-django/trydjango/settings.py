578e50f5ce127863f8be4c525ff836e1ad3a1952
Kamil Wilczek <kamil.wilczek@igoriatrade.com>
2022-03-22 15:03:43 +0100

    31 - Environment Variables & dotenv

--- Raw/Python/td32/try-django/trydjango/settings.py
+++ Raw/Python/td32/try-django/trydjango/settings.py
@@ -10,23 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print("Base_DIR", BASE_DIR)
+# print("Base_DIR", BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-a#_2_hqgq8(t2bes6a%o%33a&-ql4-i7#rffyk-x&_lrccg-n-"
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", "django-insecure-a#_2_hqgq8(t2bes6a%o%33a&-ql4-i7#rffyk-x&_lrccg-n-"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DEBUG")) == "1"  # 1 == True
 
 ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get("ALLOWED_HOSTS")]
 
 
 # Application definition
