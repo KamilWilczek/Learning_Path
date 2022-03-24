30e348fef279d7b5d6a78cdc6775424b5b94feff
Kamil Wilczek <kamil.wilczek@igoriatrade.com>
2022-03-24 15:57:09 +0100

    42 not finished 5:40

--- Raw/Python/td32/try-django/articles/utils.py
+++ Raw/Python/td32/try-django/articles/utils.py
@@ -7,17 +7,14 @@ def slugify_instance_title(instance, save=False, new_slug=None):
         slug = new_slug
     else:
         slug = slugify(instance.title)
-
     Klass = instance.__class__
     qs = Klass.objects.filter(slug=slug).exclude(id=instance.id)
-
     if qs.exists():
         # auto generate new slug
-        rand_int = random.randint(300_000, 500_000)  # one can add random characters
+        rand_int = random.randint(300_000, 500_000)
         slug = f"{slug}-{rand_int}"
         return slugify_instance_title(instance, save=save, new_slug=slug)
-
     instance.slug = slug
-
     if save:
         instance.save()
+    return instance
