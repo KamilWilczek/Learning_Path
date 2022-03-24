e0e4744cbabf4fdfc4db95284873039d52d857f5
Kamil Wilczek <kamil.wilczek@igoriatrade.com>
2022-03-24 12:44:12 +0100

    36 - Making Changes to Models & Fields

--- Raw/Python/td32/try-django/articles/models.py
+++ Raw/Python/td32/try-django/articles/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Article(models.Model):
-    title = models.TextField()
+    # https://docs.djangoproject.com/en/4.0/topics/db/models/#field-types
+    title = models.CharField(max_length=120)
     content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    publish = models.DateField(
+        auto_now_add=False, auto_now=False, null=True, blank=True
+    )
