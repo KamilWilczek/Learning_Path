a6cd2432cc1f22a64ca75be99a37a5ff06358a56
Kamil Wilczek <kamil.wilczek@igoriatrade.com>
2022-03-29 09:48:01 +0200

    58 - Django urls, includes, and app name - fix

--- Raw/Python/td32/try-django/articles/urls.py
+++ Raw/Python/td32/try-django/articles/urls.py
@@ -6,6 +6,6 @@ app_name = "articles"
 
 urlpatterns = [
     path("", article_search_view, name="search"),
-    path("create/", article_create_view, name="article-create"),
-    path("<slug:slug>/", article_detail_view, name="article-detail"),
+    path("create/", article_create_view, name="create"),
+    path("<slug:slug>/", article_detail_view, name="detail"),
 ]
