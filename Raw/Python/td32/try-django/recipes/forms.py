fbc8e1123fd437fc5c558e7b32922b014da3d95e
Kamil Wilczek <kamil.wilczek@igoriatrade.com>
2022-04-01 08:49:22 +0200

    6:42 / 6:48
    69 - Auto Save with HTMX & Django

--- Raw/Python/td32/try-django/recipes/forms.py
+++ Raw/Python/td32/try-django/recipes/forms.py
@@ -24,7 +24,14 @@ class RecipeForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         # django-cripsy-forms
         for field in self.fields:
-            new_data = {"placeholder": f"Recipe {str(field)}", "class": "form-control"}
+            new_data = {
+                "placeholder": f"Recipe {str(field)}",
+                "class": "form-control",
+                "hx-post": ".",
+                "hx-trigger": "keyup changed delay:500ms",
+                "hx-target": "#recipe-container",
+                "hx-swap": "outerHTML",
+            }
             self.fields[str(field)].widget.attrs.update(new_data)
         # self.fields["name"].label = ""
         # self.fields["name"].widget.attrs.update(
