d80ef9e4a88c035b186be2a490f45f8985e4c3a0
Kamil Wilczek <kamil.wilczek@igoriatrade.com>
2022-04-06 12:36:44 +0200

    without itertools

--- Raw/Python/codewars/kartezjan_itertools.py
+++ Raw/Python/codewars/kartezjan_itertools.py
@@ -1,25 +1,21 @@
-from itertools import product
-
 input_data = {
     "szerokość": {"20cm", "16cm", "10cm"},
     "wysokość": {"30cm", "40cm"},
     "kolor": {"czarny", "biały", "zielony"},
 }
 
-result = (
-    dict(zip(input_data.keys(), values)) for values in product(*input_data.values())
-)
+pusta_lista = []
+
+
+zbiory = [tuple(zbior) for zbior in input_data.values()]
+wynik = [[]]
+for zbior in zbiory:
+    wynik = [x + [y] for x in wynik for y in zbior]
+for something in wynik:
+    pusta_lista.append(something)
+
+print(pusta_lista)
+
+result = (dict(zip(input_data.keys(), values)) for values in pusta_lista)
 for a in result:
     print(a)
-
-# def my_product(inp):
-#     return (dict(zip(inp.keys(), values)) for values in product(*inp.values())
-
-
-def product(*args, repeat=1):
-    zbiory = [tuple(zbior) for zbior in input_data.values()]
-    wynik = [[]]
-    for zbior in zbiory:
-        wynik = [x + [y] for x in wynik for y in zbior]
-    for something in wynik:
-        yield tuple(something)
