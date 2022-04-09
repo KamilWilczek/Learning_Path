615b19f9e503288ba8e1dcab2833633faa6622d1
Kamil Wilczek <kamilwilczek@wp.pl>
2022-04-09 22:41:46 +0200

    implementing django rest framework

--- Raw/Python/study_budy/studybudy/settings.py
+++ Raw/Python/study_budy/studybudy/settings.py
@@ -40,12 +40,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # my own
     "base.apps.BaseConfig",  # 'base' also will work
+    # 3rd party
+    "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -129,3 +134,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
