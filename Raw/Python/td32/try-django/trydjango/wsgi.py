9fc6e7321895ea26934a28d7391873c98b30a908
Kamil Wilczek <kamil.wilczek@igoriatrade.com>
2022-04-12 11:31:21 +0200

    trydjango to try-django

--- Raw/Python/td32/try-django/trydjango/wsgi.py
+++ Raw/Python/td32/try-django/trydjango/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "try-django.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trydjango.settings")
 
 application = get_wsgi_application()
