19eef02c3f09c39f62cd0568ee4bdf61a1538a8b
Kamil Wilczek <kamil.wilczek@igoriatrade.com>
2022-04-12 14:19:06 +0200

    removing random id from home view in trydjango views

--- Raw/Python/td32/try-django/trydjango/views.py
+++ Raw/Python/td32/try-django/trydjango/views.py
@@ -3,40 +3,29 @@ To render html web pages
 """
 import random
 from django.http import HttpResponse
-from django.template.loader import render_to_string, get_template
+from django.template.loader import render_to_string
 from articles.models import Article
 
 
 def home_view(request, *args, **kwargs):
     """
-    Take in a request (Django sedns request)
+    Take in a request (Django sends request)
     Return HTML as a response (We pick to return the response)
     """
     name = "Justin"  # hard coded
     random_id = random.randint(1, 4)  # pseudo random
 
     # from the database??
-    article_obj = Article.objects.get(id=random_id)
+    article_obj = Article.objects.all().first()
     article_queryset = Article.objects.all()
-
     context = {
         "object_list": article_queryset,
         "object": article_obj,
-        "title": article_obj.title,
-        "id": article_obj.id,
-        "content": article_obj.content,
     }
-
-    # Django templates
-    # tmpl = get_template('home-view.html')
-    # tmpl_string = tmpl.render(context=context)
-
+    # Django Templates
     HTML_STRING = render_to_string("home-view.html", context=context)
     # HTML_STRING = """
-    # <h1>Hello {title} (id: {id})!</h1>
-    # <p>Hi {content}!</p>
-    # """.format(
-    #     **context
-    # )
-
+    # <h1>{title} (id: {id})!</h1>
+    # <p>{content}!</p>
+    # """.format(**context)
     return HttpResponse(HTML_STRING)
