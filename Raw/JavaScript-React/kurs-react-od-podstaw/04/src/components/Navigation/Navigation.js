ef884ad3b6857439b796bbafe46c03ce83617ac1
Kamil Wilczek <kamil.wilczek@igoriatrade.com>
2022-04-15 12:34:56 +0200

    cdn

--- Raw/JavaScript-React/kurs-react-od-podstaw/04/src/components/Navigation/Navigation.js
+++ Raw/JavaScript-React/kurs-react-od-podstaw/04/src/components/Navigation/Navigation.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import styles from './Navigation.module.scss';
 
 const Navigation = () => (
-    <ul>
-        <li><Link to="/">Twitters</Link></li>
-        <li><Link to="/articles">Articles</Link></li>
-        <li><Link to="/notes">Notes</Link></li>
+    <ul className={styles.wrapper}>
+        <li className={styles.navigationItem}><Link to="/">Twitters</Link></li>
+        <li className={styles.navigationItem}><Link to="/articles">Articles</Link></li>
+        <li className={styles.navigationItem}><Link to="/notes">Notes</Link></li>
     </ul>
 )
 
