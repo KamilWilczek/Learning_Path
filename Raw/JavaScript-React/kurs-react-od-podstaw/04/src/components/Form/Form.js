2e7fad4e9c28ee85faa24ded16b7f24e04126c0e
Kamil Wilczek <kamilwilczek@wp.pl>
2022-04-19 09:59:34 +0200

    Tworzenie dynamicznego formularza

--- Raw/JavaScript-React/kurs-react-od-podstaw/04/src/components/Form/Form.js
+++ Raw/JavaScript-React/kurs-react-od-podstaw/04/src/components/Form/Form.js
@@ -4,31 +4,76 @@ import Input from "../Input/Input";
 import Button from '../Button/Button';
 import Title from '../Title/Title';
 
-const Form = ({ submitFn }) => (
-  <div className={styles.wrapper}>
-    <Title>Add new twitter account</Title>
-    <form autoComplete="off" className={styles.form} onSubmit={submitFn}>
-      <Input
-        name="name"
-        label="Name"
-        maxLength={30}
-      />
-      <Input
-        name="link"
-        label="Twitter link"
-      />
-      <Input
-        name="image"
-        label="Image"
-      />
-      <Input
-        tag="textarea"
-        name="description"
-        label="Description"
-      />
-      <Button>add new item</Button>
-    </form>
-  </div>
-);
+const types = {
+  twitter: 'twitter',
+  article: 'article',
+  note: 'note',
+}
+
+const descriptions = {
+  twitter: 'favorite Twitter account',
+  article: 'Article',
+  note: 'Note',
+}
+
+class Form extends React.Component {
+
+  state = {
+    activeOption: types.twitter,
+  }
+
+  handleRadioButtonChange = (type) => {
+    this.setState({
+      activeOption: type,
+    })
+  }
+
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <Title>Add new {descriptions[this.state.activeOption]}</Title>
+        <form autoComplete="off" className={styles.form} onSubmit={this.submitFn}>
+          <input
+            id={types.twitter}
+            type='radio'
+            checked={this.state.activeOption === types.twitter}
+            onChange={() => this.handleRadioButtonChange(types.twitter)}
+          />
+          <input
+            id={types.article}
+            type='radio'
+            checked={this.state.activeOption === types.article}
+            onChange={() => this.handleRadioButtonChange(types.article)}
+          />
+          <input
+            id={types.note}
+            type='radio'
+            checked={this.state.activeOption === types.note}
+            onChange={() => this.handleRadioButtonChange(types.note)}
+          />
+          <Input
+            name="name"
+            label="Name"
+            maxLength={30}
+          />
+          <Input
+            name="link"
+            label="Twitter link"
+          />
+          <Input
+            name="image"
+            label="Image"
+          />
+          <Input
+            tag="textarea"
+            name="description"
+            label="Description"
+          />
+          <Button>add new item</Button>
+        </form>
+      </div>
+    )
+  }
+};
 
 export default Form;
