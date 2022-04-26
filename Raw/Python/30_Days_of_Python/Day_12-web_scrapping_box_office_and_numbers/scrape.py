839115042f972f48bf86569391b956905f8afe17
Kamil Wilczek <kamilwilczek@wp.pl>
2022-04-26 23:04:16 +0200

    Day_12 start

---
+++ Raw/Python/30_Days_of_Python/Day_12-web_scrapping_box_office_and_numbers/scrape.py
@@ -0,0 +1,24 @@
+import datetime
+import requests
+from requests_html import HTML
+
+now = datetime.datetime.now()
+year = now.year
+
+
+def url_to_txt(url, filename="world.html", save=False):
+    r = requests.get(url)
+    if r.status_code == 200:
+        html_text = r.text
+        if save:
+            with open(f"world-{year}.html", "w", encoding="utf-8") as f:
+                f.write(html_text)
+        return html_text
+    return ""
+
+
+url = "https://www.boxofficemojo.com/year/world/"
+
+html_text = url_to_txt(url)
+
+r_html = HTML(html=html_text)
